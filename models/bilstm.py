3ad0ce9daf8e2e91b179ca56d814bd2c7bbd169b
RubenvanHeusden <rubenbaseball35@gmail.com>
2020-06-12 20:04:58 +0200

    added model skeletons for LSTM and CNN

---
+++ models/bilstm.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+
+class BiLSTM(nn.Module):
+    def __init__(self, vocab, hidden_dim: int, output_dim: int, dropout: float = 0.3,
+                 device=torch.device("cpu"), use_lengths=True):
+        """
+        @param vocab: a vector containing the word embeddings of the word in the train set
+        @param hidden_dim: int specifying number of hidden units in LSTM
+        @param output_dim: int specifying the number of output units
+        @param dropout: float specifying the dropout ratio
+        @param device: torch.device specifying if model is ran on cpu or gpu
+        @param use_lengths: boolean specifying whether to remove padding for LSTM input or not
+        """
+        super(BiLSTM, self).__init__()
+        self.embed = nn.Embedding(*vocab.shape)
+        self.embed.weight.data.copy_(vocab)
+        self.embedding_dim = vocab.shape[1]
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+        self.lstm = nn.LSTM(self.embedding_dim, hidden_dim, batch_first=True, bidirectional=True)
+        self.fc_out = nn.Linear(hidden_dim*2, output_dim)
+        self.dropout = nn.Dropout(dropout)
+        self.device = device
+        self.use_lengths = use_lengths
+
+    def forward(self, x):
+        """
+        @param x: tensor of size (batch_size, seq_length)
+        @return:
+        """
+        if isinstance(x, list):
+            inputs, lengths = x
+            b = inputs.shape[0]
+            inputs = self.embed(inputs)
+            x = nn.utils.rnn.pack_padded_sequence(inputs, lengths, batch_first=True, enforce_sorted=False)
+        else:
+            b = x.shape[0]
+            x = self.embed(x)
+
+        h_0 = torch.zeros(2, b, self.hidden_dim).to(self.device)
+        c_0 = torch.zeros(2, b, self.hidden_dim).to(self.device)
+
+        torch.nn.init.xavier_normal_(h_0)
+        torch.nn.init.xavier_normal_(c_0)
+
+        output, (final_hidden_state, final_cell_state) = self.lstm(x, (h_0, c_0))
+        final_hidden_state = torch.cat([final_hidden_state[0, :, :], final_hidden_state[1, :, :]], dim=1)
+
+        del x
+        if self.use_lengths:
+            del inputs
+            del lengths
+        del b
+        del h_0
+        del c_0
+        return self.fc_out(self.dropout(final_hidden_state))
---
+++ models/cnn.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+from typing import List
+
+
+class CNN(nn.Module):
+    """
+    This class implements a basic convolutional neural network for
+    text classification as presented in (Kim,. 2014)
+
+    """
+    def __init__(self, input_channels: int, output_dim: int, filter_list: List[int],
+                 embed_matrix, num_filters: int, dropbout_probs: float = 0.5, use_bert_embeds: bool = False):
+        """
+
+        @param input_channels: integer specifying the number of input channels of the 'image'
+
+        @param output_dim: integer specifying the number of outputs
+
+        @param filter_list: list of integers specifying the size of each kernel that is applied to the image,
+        the outputs of the kernels are concatenated to form the input to the linear layer
+
+        @param embed_matrix: torch Tensor with size [size_vocab, embedding_dim] where each row is a
+        word embedding
+
+        @param num_filters: the amount of filter to apply to the image, this also determins the size
+        of the input to the fully connected layers, which is equal to num_kernels*num_filters
+
+        @param dropbout_probs: float specifying the dropout probabilities
+        """
+        super(CNN, self).__init__()
+        self.params = locals()
+        self.input_channels = input_channels
+        self.output_dim = output_dim
+        self.filters = nn.ModuleList([nn.Conv2d(in_channels=1, out_channels=num_filters,
+                                                kernel_size=(n, embed_matrix.shape[1])) for n in filter_list])
+
+        self.max_over_time_pool = torch.nn.AdaptiveMaxPool2d(1, return_indices=False)
+        self.fc_layer = nn.Linear(num_filters*len(filter_list), output_dim)
+        self.dropout = nn.Dropout(p=dropbout_probs)
+        self.embed = nn.Embedding(*embed_matrix.shape)
+        self.embed.weight.data.copy_(embed_matrix)
+        self.embed.requires_grad = True
+        self.relu = nn.ReLU()
+        self.use_bert_embeddings = use_bert_embeds
+
+    def forward(self, x):
+        """
+        @param x: input of size (batch_size, max_sen_length_batch)
+        @return: output of the CNN applied to the input x
+        """
+        if isinstance(x, list):
+            x = x[0]
+        x = x.unsqueeze(1)
+        if not self.use_bert_embeddings:
+            x = self.embed(x)
+        filter_outs = []
+        for module in self.filters:
+            module_out = self.relu(module(x))
+            module_out = self.max_over_time_pool(module_out)
+            filter_outs.append(module_out)
+        pen_ultimate_layer = torch.cat(filter_outs, dim=1)
+        output = self.dropout(pen_ultimate_layer).squeeze()
+        output = self.fc_layer(output)
+
+        return output
